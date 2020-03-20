3dd16f0e5ed331b03bc63f58f0983ead3721fee9
Jo Keizer <jolanda@sociossports.nl>
2020-03-20 10:45:03 +0100

    work in progress

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,3 @@
-console.log('JavaScript file successfully served and linked!');
-
-// const btn = document.querySelector('button');
-// const body = document.querySelector('body');
-//
-// btn.addEventListener( 'click', function() {
-//   body.classList.toggle('darkmode');
-//   this.classList.toggle('btn-darkmode');
-//   if (this.textContent === 'Dark Mode') {
-//     this.textContent = 'Light Mode';
-//   } else {
-//     this.textContent = 'Dark Mode';
-//   }
-// });
-//
-//
-
 $( document ).ready(function() {
   $(".menu-toggle").on("click", function() {
     $(this).toggleClass("on");
@@ -22,4 +5,62 @@ $( document ).ready(function() {
     $("nav ul").toggleClass("hidden");
   });
 
+
+  var movementStrength = 30;
+  var height = movementStrength / $(window).height();
+  var width = movementStrength / $(window).width();
+  $("#top-image").mousemove(function(e){
+    var pageX = e.pageX - ($(window).width() / 2);
+    var pageY = e.pageY - ($(window).height() / 2);
+    var newvalueX = width * pageX * -4 - 25;
+    var newvalueY = height * pageY * -4 - 50;
+    $('#top-image').css("background-position", newvalueX+"px     "+newvalueY+"px");
+  });
+
 });
+
+
+//Facebook
+let rotateFB = document.getElementById( 'rotateFB' );
+
+rotateFB.addEventListener( 'mouseover', function () {
+  this.className = 'over';
+}, false );
+
+rotateFB.addEventListener( 'mouseout', function () {
+  let rotateFB = this;
+  rotateFB.className = 'out';
+  window.setTimeout( function () { rotateFB.className = '' }, 550 );
+
+}, false );
+
+
+//Twitter
+let rotateTw = document.getElementById( 'rotateTw' );
+
+rotateTw.addEventListener( 'mouseover', function () {
+  this.className = 'over';
+}, false );
+
+rotateTw.addEventListener( 'mouseout', function () {
+  let rotateTw = this;
+  rotateTw.className = 'out';
+  window.setTimeout( function () { rotateTw.className = '' }, 550 );
+
+}, false );
+
+//Instagram
+
+let rotateIg = document.getElementById( 'rotateIg' );
+
+rotateIg.addEventListener( 'mouseover', function () {
+  this.className = 'over';
+}, false );
+
+rotateIg.addEventListener( 'mouseout', function () {
+  let rotateIg = this;
+  rotateIg.className = 'out';
+  window.setTimeout( function () { rotateIg.className = '' }, 550 );
+
+}, false );
+
